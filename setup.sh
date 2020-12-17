76fadf8a46e664a60702aa05de5821e96f31a6f5
Alexander Herlin <alexander.herlin@outlook.com>
2020-12-17 15:53:26 +0100

    Changed folder names

--- setup.sh
+++ setup.sh
@@ -4,13 +4,13 @@ brew install protobuf
 git clone https://github.com/grpc/grpc-swift.git temp-swift-grpc
 cd temp-swift-grpc
 make plugins
-cp protoc-gen-swift protoc-gen-grpc-swift ../Dela.App
+cp protoc-gen-swift protoc-gen-grpc-swift ../Dela_App
 cd ..
 rm -rf temp-swift-grpc
 
 mkdir 
 protoc helloworld.proto \
      --proto_path=./Proto \
-     --swift_out=Visibility=Public:./Dela.App/Shared/GenGrpcClient \
-     --grpc-swift_out=Visibility=Public,Client=true,Server=false:./Dela.App/Shared/GenGrpcClient \
-     --plugin=./Dela.App/protoc-gen-grpc-swift
+     --swift_out=Visibility=Public:./Dela_App/Shared/GenGrpcClient \
+     --grpc-swift_out=Visibility=Public,Client=true,Server=false:./Dela_App/Shared/GenGrpcClient \
+     --plugin=./Dela_App/protoc-gen-grpc-swift
