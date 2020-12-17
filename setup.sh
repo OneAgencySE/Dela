feefd981bbad48aa96294874fc6765aaa93fca27
Joacim Nidén <joacim.niden@oneagency.se>
2020-12-17 16:11:44 +0100

    Update setup script

--- setup.sh
+++ setup.sh
@@ -8,9 +8,10 @@ cp protoc-gen-swift protoc-gen-grpc-swift ../Dela_App
 cd ..
 rm -rf temp-swift-grpc
 
-mkdir 
+mkdir ./Dela_App/Shared/GenGrpcClient
 protoc helloworld.proto \
      --proto_path=./Proto \
      --swift_out=Visibility=Public:./Dela_App/Shared/GenGrpcClient \
      --grpc-swift_out=Visibility=Public,Client=true,Server=false:./Dela_App/Shared/GenGrpcClient \
-     --plugin=./Dela_App/protoc-gen-grpc-swift
+     --plugin=./Dela_App/protoc-gen-grpc-swift \
+     --plugin=./Dela_App/protoc-gen-swift
