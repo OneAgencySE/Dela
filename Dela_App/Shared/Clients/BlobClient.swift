a2ea6fac456ac556c3b9e00643f23319353efc80
Alexander Herlin <alexander.herlin@outlook.com>
2021-01-13 13:55:06 +0100

    Upload working

--- Dela_App/Shared/Clients/BlobClient.swift
+++ Dela_App/Shared/Clients/BlobClient.swift
@@ -30,33 +30,34 @@ class BlobClient {
     }
 
     func uploadImge(data: Data) -> AnyPublisher<BlobInfo, UserInfoError> {
-
         Future<BlobInfo, UserInfoError> { promise in
+            let upload = self.client.upload()
 
-            var request = Array(data)
+            let chunks = Array(data)
                 .chunked(into: 1024)
                 .map { chunk in
                     Blob_BlobData.with {
                         $0.chunkData = Data(chunk)
                     }
                 }
+            upload.sendMessages(chunks, promise: nil)
 
-            request.append(Blob_BlobData.with {
+            let imageInfo = Blob_BlobData.with {
                 $0.info = Blob_FileInfo.with {
                     $0.extension = ".jpeg"
                     $0.metaText = "flowa-powa"
                     $0.fileName = ""
                 }
-            })
-
-            let upload = self.client.upload()
-            upload.sendMessages(request, promise: nil)
+            }
+            upload.sendMessage(imageInfo, promise: nil)
 
             do {
+                upload.sendEnd(promise: nil)
                 let response = try upload.response.wait()
                 promise(.success(BlobInfo(blobId: response.blobID)))
             } catch {
-                promise(.failure(.communication(UserInfoError.defaultComMsg)))
+                // TODO: Log Error to server
+                promise(.failure(.communication(error.localizedDescription)))
             }
 
         }.eraseToAnyPublisher()
