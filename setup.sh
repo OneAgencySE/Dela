621c7ceb69ece4cc06d02a489e683ac9efacfa29
Alexander Herlin <alexander.herlin@outlook.com>
2021-01-14 10:52:30 +0100

    Install Swiftlint on setup

--- setup.sh
+++ setup.sh
@@ -1,16 +1,23 @@
 #!/bin/sh
 
-if brew ls --versions protobuf > /dev/null; then
-     echo "protobuf is installed"
+if brew ls --versions swiftlint > /dev/null; then
+     echo "Swiftlint is installed!"
 else
-     echo "Installing needed depencencies..."
+     echo "Installing Swiftlint..."
+     brew install swiftlint
+fi
+
+if brew ls --versions protobuf > /dev/null; then
+     echo "Protobuf is installed!"
+else
+     echo "Installing Protobuf..."
      brew install protobuf
 fi
 
 if [[ -f ./Dela_App/protoc-gen-grpc-swift ]] && [[ -f ./Dela_App/protoc-gen-swift ]]; then 
-     echo "protoc generator files exists"
+     echo "Protoc generator files exists"
 else
-     echo "Installing protoc generator files..."
+     echo "Building Protoc generator files..."
      git clone https://github.com/grpc/grpc-swift.git temp-swift-grpc
      cd temp-swift-grpc
      make plugins
@@ -26,16 +33,16 @@ sh ./Scripts/generateproto.sh
 cd ..
 
 if [[ -f ./Dela_App/local.xcconfig ]]; then 
-     echo "local.xcconfig exists"
+     echo "Settings file local.xcconfig exists"
 else    
      echo 'API_URL = <ADDRESS>' > ./Dela_App/local.xcconfig
      echo "Be sure to check out the local.xcconfig file"
 fi 
 
 if [[ -f ./Dela_Backend/.env ]]; then 
-     echo "backend .env exists"
+     echo "The backend .env exists"
 else
-     echo "Be sure to check out the .env file"
+     echo "Be sure to check out the backend .env file"
      echo 'TEST_JPEG=test_img.jpeg\nUPLOAD_PATH=Upload\nSERVER_ADDR=0.0.0.0:50051' > ./Dela_Backend/.env
 fi
 
